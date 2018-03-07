df08511c749aa342d4a76a56e8996e582d948884
Deyvison Rocha <deyvison@gmail.com>
2018-03-06 22:11:48 -0300

    Fix cmd.sh

--- config/cmd.sh
+++ config/cmd.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-NGINX_ROOT=${NGINX_ROOT:=/var/www/app}
-
 # Display PHP error's or not
 sed -i -e "s/error_reporting =.*=/error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT/g" /etc/php/7.1/fpm/php.ini
 sed -i -e "s/display_errors =.*/display_errors = Off/g" /etc/php/7.1/fpm/php.ini
@@ -12,12 +10,9 @@ sed -i -e "s/display_errors =.*/display_errors = Off/g" /etc/php/7.1/fpm/php.ini
 procs=$(cat /proc/cpuinfo |grep processor | wc -l)
 sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
 
-# Set the root in the conf
-sed -i -e "s#%%NGINX_ROOT%%#$NGINX_ROOT#" /etc/nginx/sites-available/default.conf
-
 # Again set the right permissions (needed when mounting from a volume)
 set +e
-chown -Rf www-data.www-data $NGINX_ROOT
+chown -Rf www-data.www-data /var/www/app
 set -e
 
 # if commands are entered for crontab - check it
